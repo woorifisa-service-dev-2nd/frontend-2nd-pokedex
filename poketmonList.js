867af73c9e91363e15660561a97619c5dbacecc1
mimimya <sj.ella1030@gmail.com>
2023-11-30 16:06:46 +0900

    리스트 가져와 포켓몬 객체 출력 완성

--- poketmonList.js
+++ poketmonList.js
@@ -1,51 +1,71 @@
-const text = document.getElementById('poketmon');
-const img = document.getElementById('img');
+const text = document.getElementById(`poketmon`);
+const img = document.getElementById(`img`);
 const optionsFrom = (method, body, headers) => {
-    method,
-        body,
-        headers
-}
-//전체 포켓몬을 줄세워 데려오기
-const getPocketmonList = () => {
-    url = `https://pokeapi.co/api/v2/pokemon?limit=1292&offset=0`; //id : 0~1291
-    const options = optionsFrom(
-        'GET',
-    )
-    //프로미스 공부 필요;;저도 이해가 안가요
-    const array = fetch(url, options)
-        .then((response) => { return response.json(); })//promise, return
-        .then((object) => { console.log(object.results); });//{name, url} Array: *1292
-}
+  method, body, headers;
+};
+// 전체 포켓몬을 줄세워 데려오기
+const getPocketmonList = (start, end) => {
+  // url = `https://pokeapi.co/api/v2/pokemon?limit=1292&offset=0`; //id : 0~1291
+  url = `https://pokeapi.co/api/v2/pokemon?limit=${end}&offset=${start}`;
+  const options = optionsFrom(`GET`);
+  // 프로미스 공부 필요;;저도 이해가 안가요
+  const array = fetch(url, options)
+    .then((response) => {
+      return response.json();
+    }) // promise, return
+    .then((object) => {
+      return object.results.map((obj) => {
+        return getItem(obj.name);
+      });
+    });
 
+  // {name, url} Array: *1292
+  return array;
+};
 
-const getPocketFrom = (name, url) => {
-    gUrl = `https://pokeapi.co/api/v2/pokemon/${name}`
-    const options = optionsFrom(
-        'GET',
-    )
-    //url -> "front_default" 이미지 png, gif 선택
-    fetch(url, options)
-        .then((response) => { console.log(response.json()); })
-    //.then((object) => {console.log(object.results)})
-    return
-    //사진
-    //이름
-    //속성
-    //종류
-    //신장
-    //무게
-}
+// 해당 id의
 
-function isJson(str) {
-    try {
-        JSON.parse(str);
-    } catch (e) {
-        return false;
-    }
-    return true;
-}
-//onst object = [getPocketmonList()];
-const pocketMonArray = getPocketmonList().then((ob) => { console.log(ob); })
-console.log(pocketMonArray);
-//console.log(typeof object[0]);
-//getPocketFrom(getPocketmonList()[0].name);
+const getItem = (name) => {
+  // 설명 가져오기
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    .then((spInfo) => spInfo.json())
+    .then((o) => {
+      // console.log(o);
+      const PocketMon = {
+        new_url: o.species.url,
+        id: o.id,
+        name: `이름`,
+        flavorText: ``,
+        genera: `종`,
+        types: o.types,
+        img:
+          o?.sprites?.versions?.[`generation-v`]?.[`black-white`]?.animated
+            ?.front_default || o?.sprites?.front_default,
+        height: o.height,
+        weight: o.weight,
+      };
+
+      return PocketMon;
+    })
+    .then((PocketMon) => {
+      fetch(PocketMon.new_url, { method: `GET` })
+        .then((spInfo) => spInfo.json())
+        .then((o) => {
+            
+            PocketMon.flavorText = o.flavor_text_entries.filter(
+                (v) => v.language.name === `ko`
+                )[0].flavor_text;
+                
+                PocketMon.name = o.names[2].name;
+                PocketMon.genera = o.genera[1].genus;
+                console.log(PocketMon);
+          // console.log(
+          //   `id ${o.id} text ${koreanFlavorText[0].flavor_text}, names ${names[2].name} genreko ${generaKo[1].genus}`
+          // );
+
+          return true;
+        });
+    });
+};
+
+getPocketmonList(0, 750); // id 386~ 진화종 1291
