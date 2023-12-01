9174f01b76f20b44cc58d38080a89221fcbd0fa8
sws6641 <sws6641@naver.com>
2023-12-02 00:14:28 +0900

    기능 최종 완료/css 추가

--- app.js
+++ app.js
@@ -1,198 +1,149 @@
 import { pokomonList } from "./poketmonList.js";
-
-// const testlist = pokomonList[0];
-
-const map = new Map();
-
-map.set(`1`, `bulbasaur`);
-map.set(`이상해씨`, `bulbasaur`);
-map.set(`bulbasaur`, `이상해씨`);
-map.set(`4`, `charmander`);
-map.set(`파이리`, `charmander`);
-map.set(`charmander`, `파이리`);
-map.set(`7`, `squirtle`);
-map.set(`꼬부기`, `squirtle`);
-map.set(`squirtle`, `꼬부기`);
-
 const searchText = document.getElementById(`searchText`);
 const searchBtn = document.getElementById(`searchBtn`);
-
-// const pokemonName = document.getElementById(`pokemonName`);
-// const pokemonId = document.getElementById(`id`);
-// const image = document.getElementById(`image`);
-// const type1 = document.getElementById(`type1`);
-// const type2 = document.getElementById(`type2`);
-
 const pokemonName = document.getElementsByClassName(`pokemonName`);
 const pokemonId = document.getElementsByClassName(`id`);
 const image = document.getElementsByClassName(`image`);
 const type1 = document.getElementsByClassName(`type1`);
 const type2 = document.getElementsByClassName(`type2`);
-
+// 검색 돋보기 클릭 이벤트
 searchBtn.addEventListener(`click`, () => {
+    searchEventAll();
+});
+// 검색 엔터키 이벤트
+searchText.addEventListener('keyup', () => {
+    if (window.event.keyCode === 13) {
+        // 엔터키가 눌렸을 때
+        searchEventAll();
+    }
+})
+// 클릭, 엔터 이벤트 공통
+const searchEventAll = () => {
     const idOrName = searchText.value;
+    const pokemonListArea = document.getElementById(`pokemonListArea`);
+    pokemonListArea.innerHTML = ``;
 
-    const te = document.getElementById(`pokemonListArea`);
-    te.innerHTML = ``;
-
-    console.log(pokomonList[0]);
-
-    console.log(idOrName);
-
-    const searchPokemon = pokomonList.filter(
-        (element) =>
-            element.name === idOrName || element.id.toString() === idOrName,
-    );
-
+    // 정규식 검색, 다중 검색
     const searchResult = pokemonSearch2(idOrName);
-
-    // addHtml(searchResult);
-
     searchResult.forEach((element) => {
         addHtml(element);
     });
-
-    searchPokemon.forEach((element) => {
-        pokemonSearch(
-            element.name,
-            element.id,
-            element.types[0],
-            element.types[1],
-            element.img,
-        );
-    });
-});
-
-const getPekemonInfo = async (idOrName) => {
-    let test;
-    await fetch(`https://pokeapi.co/api/v2/pokemon/${idOrName}`, {
-        method: `GET`,
-    })
-        .then((res) => res.json())
-        .then((res) => {
-            const img =
-                res?.sprites?.versions?.[`generation-v`]?.[`black-white`]
-                    ?.animated?.front_default || res?.sprites?.front_default;
-
-            let type1 = ``;
-            let type2 = ``;
-            if (res.types.length === 2) {
-                type1 = res.types[0].type.name;
-                type2 = res.types[1].type.name;
-            } else if (res.types.length === 1) {
-                type1 = res.types[0].type.name;
-                type2 = ``;
-            }
-
-            test = {
-                pokemonName: res.species.name,
-                pokemonId: res.id,
-                type1,
-                type2,
-                image: img,
-            };
-        });
-
-    return test;
-};
-
-const enterkey = async () => {
-    if (window.event.keyCode === 13) {
-        // 엔터키가 눌렸을 때
-        const idOrName = searchText.value;
-
-        const item = await getPekemonInfo(`${map.get(idOrName)}`);
-
-        console.log(pokomonList);
-
-        const testlist = pokomonList[0];
-
-        pokemonSearch(
-            testlist.name,
-            testlist.id,
-            testlist.types[0],
-            testlist.types[1],
-            testlist.img,
-        );
-    }
-};
-
-const pokemonSearch = (a, b, c, d, e) => {
-    if (d !== undefined) type2[0].innerHTML = d;
-    else type2[0].innerHTML = ``;
-
-    pokemonName[0].innerHTML = a;
-    pokemonId[0].innerHTML = `# ${b}`;
-    type1[0].innerText = c;
-    // type2[0].innerHTML = d;
-    image[0].setAttribute(`src`, e);
-
-    addHtml(b);
-};
-
+}
+// 포켓몬 리스트 추가
 const addHtml = (item) => {
     const pokemonListArea = document.getElementById(`pokemonListArea`);
+    const pokeImg = `poke-ball.png`;
     const newTag = document.createElement(`div`);
     newTag.setAttribute(`class`, `pokemonCrad`);
     newTag.setAttribute(`id`, `pokemonCrad_${item.id}`);
-    newTag.innerHTML = `
-                <p class="pokemonName">${item.name}</p>
-                <p class="id"># ${item.id}</p>
-                <img class="image" src="${item.img}" alt="" /><br />
-                <span class="type1">${item.types[0]}</span>
-                <span class="type2">${item.types[1]}</span>`;
 
+    // 속성 타입 선택
+    const typeId1 = selectTypes(item.types[0]);
+    const typeId2 = selectTypes(item.types[1]);
+
+    newTag.innerHTML = `
+                <p class="pokemonName"><img src="${pokeImg}" alt="" />${item.name}</p>
+                <p class="id"># ${item.id}</p>
+                <div class="image-gif">
+                    <img class="image" src="${item.img}" alt="" /><br />
+                </div>
+                <div class="types">
+                    ${item.types.length === 1 ? `<span id="${typeId1}" class="type1" style="border: 1px solid; width: 90%; margin-left: 5%">${item.types[0]}</span>` : ``}
+                    ${item.types.length === 2 ? `<span id="${typeId1}" class="type1">${item.types[0]}</span><span id="${typeId2}" class="type2">${item.types[1]}</span>` : ``}
+                </div>
+                `;
     pokemonListArea.appendChild(newTag);
 };
+
+// 속성 타입 선택
+const selectTypes = (type) => {
+    if (type === `노말`) {
+        return `normal`;
+    }
+    else if (type === `격투`) {
+        return `fighting`;
+    }
+    else if (type === `비행`) {
+        return `flying`;
+    }
+    else if (type === `독`) {
+        return `poison`;
+    }
+    else if (type === `땅`) {
+        return `ground`;
+    }
+    else if (type === `바위`) {
+        return `rock`;
+    }
+    else if (type === `벌레`) {
+        return `bug`;
+    }
+    else if (type === `고스트`) {
+        return `ghost`;
+    }
+    else if (type === `강철`) {
+        return `steel`;
+    }
+    else if (type === `불꽃`) {
+        return `fire`;
+    }
+    else if (type === `물`) {
+        return `water`;
+    }
+    else if (type === `풀`) {
+        return `grass`;
+    }
+    else if (type === `전기`) {
+        return `electric`;
+    }
+    else if (type === `에스퍼`) {
+        return `psychic`;
+    }
+    else if (type === `얼음`) {
+        return `ice`;
+    }
+    else if (type === `드래곤`) {
+        return `dragon`;
+    }
+    else if (type === `악`) {
+        return `dark`;
+    }
+    else if (type === `페어리`) {
+        return `fairy`;
+    }
+    else {
+        return ``;
+    }
+}
 
 const pokemonSearch2 = (input) => {
     const regex = new RegExp(`${input}`, `g`);
     return pokomonList.filter((pockemon) => pockemon.name.match(regex));
 };
-
-const [
-    normalButton,
-    fightingButton,
-    flyingButton,
-    poisonButton,
-    groundButton,
-    rockButton,
-    bugButton,
-    ghostButton,
-    steelButton,
-    fireButton,
-    waterButton,
-    grassButton,
-    electricButton,
-    psychicButton,
-    iceButton,
-    dragonButton,
-    darkButton,
-    fairyButton,
-] = document.getElementsByClassName(`type_button`);
-
+const typeButtons = document.querySelectorAll(`button`);
 const typeButtonHandler = (event) => {
-    // 현재 클릭된 타입 이미지
-    console.log(event.currentTarget.childNodes[1].src);
-    // 현재 클릭된 타입 이름
-    console.log(event.currentTarget.childNodes[3].innerText);
+    // pokemonSearch(`이상해`);
+    // searchInputControl.innerHTML =
+    //     `<span class="searchTypeResult"><img src=${event.currentTarget.childNodes[1].src} width="32" height="32" />
+    //         <span>${event.currentTarget.childNodes[3].innerText}</span></span>
+    // ` +
+    //     searchText.outerHTML +
+    //     searchBtn.outerHTML;
+    const pokemonListArea = document.getElementById(`pokemonListArea`);
+    pokemonListArea.innerHTML = ``;
+    const target = pokomonList.filter((pockemon) =>
+        pockemon.types.includes(event.currentTarget.childNodes[3].innerText),
+    );
+    // 3. 아래 pokemonListArea 에 포켓몬 보여주기
+    console.log(target);
+    target.forEach((element) => {
+        addHtml(element);
+    });
 };
 
-normalButton.addEventListener(`click`, typeButtonHandler);
-fightingButton.addEventListener(`click`, typeButtonHandler);
-flyingButton.addEventListener(`click`, typeButtonHandler);
-poisonButton.addEventListener(`click`, typeButtonHandler);
-groundButton.addEventListener(`click`, typeButtonHandler);
-rockButton.addEventListener(`click`, typeButtonHandler);
-bugButton.addEventListener(`click`, typeButtonHandler);
-ghostButton.addEventListener(`click`, typeButtonHandler);
-steelButton.addEventListener(`click`, typeButtonHandler);
-fireButton.addEventListener(`click`, typeButtonHandler);
-waterButton.addEventListener(`click`, typeButtonHandler);
-grassButton.addEventListener(`click`, typeButtonHandler);
-electricButton.addEventListener(`click`, typeButtonHandler);
-psychicButton.addEventListener(`click`, typeButtonHandler);
-iceButton.addEventListener(`click`, typeButtonHandler);
-dragonButton.addEventListener(`click`, typeButtonHandler);
-darkButton.addEventListener(`click`, typeButtonHandler);
-fairyButton.addEventListener(`click`, typeButtonHandler);
+typeButtons.forEach(button => {
+    if (button.id !== `searchBtn`) {
+        // console.log(button.id);
+        button.addEventListener(`click`, typeButtonHandler);
+    }
+});
