1044a824d251eca08fce970a94a5d1e1f544c750
eunhye park <lime1pm@naver.com>
2023-12-03 19:36:49 +0900

    Feat detail modal

--- public/app.js
+++ public/app.js
@@ -57,6 +57,7 @@ const makeCard = (item) => {
     const newTag = document.createElement(`div`);
     newTag.setAttribute(`class`, `pokemonCard`);
     newTag.setAttribute(`id`, `pokemonCard_${item.id}`);
+    newTag.addEventListener(`click`, cardClickHandler);
 
     // 속성 타입 선택
     const typeClass1 = selectTypes(item.types[0]);
@@ -89,6 +90,73 @@ const makeCard = (item) => {
     return newTag;
     /* eslint-enable indent */
 };
+
+const cardClickHandler = (e) => {
+    const id = e.currentTarget.id.replace(/[^0-9]/g, ``);
+    const pokemon = pokemonList.find((p) => p.id.toString() === id);
+
+    displayDetailCard(pokemon);
+};
+
+const background = document.querySelector(`#background`);
+const detailContainer = document.getElementById(`detail-container`);
+const detailGenera = detailContainer.querySelector(`#genera`);
+const detailId = document.querySelector(`#detail-id`);
+const mainImg = document.querySelector(`#main-image`);
+const detailName = document.querySelector(`#detail-name`);
+const detailDesc = document.querySelector(`#detail-desc`);
+const detailHeight = document.querySelector(`#height`);
+const detailWeight = document.querySelector(`#weight`);
+const detailImgs = detailContainer.querySelector(`#image-wrap`);
+
+const displayDetailCard = (pokemon) => {
+    const detailTypes = document.createElement(`div`);
+    detailTypes.id = `type-wrap`;
+
+    pokemon.types.forEach((type) => {
+        const enType = selectTypes(type);
+
+        const typeTag = document.createElement(`div`);
+        typeTag.id = `type-${enType}`;
+        typeTag.classList.add(`detail-type`);
+        typeTag.classList.add(enType);
+        typeTag.innerHTML = `<img class="detail-type-icon" src="images/type-images/${enType}.svg"/>${type}`;
+
+        detailTypes.appendChild(typeTag);
+    });
+
+    detailGenera.innerText = pokemon.genera;
+    detailGenera.appendChild(detailTypes);
+
+    detailId.innerText = `No.${String(pokemon.id).padStart(4, `0`)}`;
+
+    mainImg.src = pokemon.img;
+
+    detailName.innerText = pokemon.name;
+
+    detailDesc.innerText = pokemon.flavorText.replace(/\n/g, ` `);
+
+    detailHeight.innerText = pokemon.height;
+
+    detailWeight.innerText = pokemon.weight;
+
+    pokemon.imgs.forEach((img) => {
+        detailImgs.innerHTML += `<img class="detail-image" src=${img}>`;
+    });
+
+    document.body.style.overflow = `hidden`;
+    detailContainer.scrollTo(0, 0);
+    background.className = `__visible`;
+    detailContainer.className = `__visible`;
+};
+
+document.getElementById(`detail-close-btn`).addEventListener(`click`, () => {
+    document.body.style.removeProperty(`overflow`);
+    background.className = `__invisible`;
+    detailContainer.className = `__invisible`;
+    detailGenera.innerHTML = ``;
+    detailImgs.innerHTML = ``;
+});
 
 // 포켓몬 카드 결과 영역에 추가
 const addHtml = (item) => {
